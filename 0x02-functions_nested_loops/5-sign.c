08f373cb544aae199d557934b11bc52156ae3d90
KingDee715 <Davidaklamanu651@gmail.com>
2023-02-22 01:15:42 +0000

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * @n: carrier variable
+ * Description: printing the sign of a number
+ * Return: 1 if n>0, 0 if n == 0, -1 if n<0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+
+	{
+		_putchar ('+');
+		return (1);
+	}
+
+	else if (n < 0)
+
+	{
+		_putchar ('-');
+		return (-1);
+	}
+
+
+	{
+		_putchar ('0');
+		return (0);
+	}
+
+}
