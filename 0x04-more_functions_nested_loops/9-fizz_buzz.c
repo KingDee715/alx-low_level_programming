448a13600fe69f6dab3376f2b052bb5bcd4ee03e
KingDee715 <Davidaklamanu651@gmail.com>
2023-02-23 23:22:38 +0000

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,8 +12,13 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 
-		if (a % 3 == 0)
+		if (a % 3 == 0 && a % 5 == 0)
 
+		{
+			printf("FizzBuzz");
+		}
+
+		else if (a % 3 == 0)
 		{
 			printf("Fizz");
 		}
@@ -21,11 +26,6 @@ int main(void)
 		else if (a % 5 == 0)
 		{
 			printf("Buzz");
-		}
-
-		else if (a % 3 == 0 && a % 5 == 0)
-		{
-			printf("FizzBuzz");
 		}
 
 		else
