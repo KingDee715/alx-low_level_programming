80e596b6713ee1043b456bb372f37ddc354a1b78
KingDee715 <Davidaklamanu651@gmail.com>
2023-03-23 05:04:47 +0000

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,16 @@
 #ifndef CALC_H
 #define CALC_H
 
+/*
+ * Desc: Header file containing all structures and
+ *       prototypes used by the 3-main.c program.
+ */
+
 /**
  * struct op - A struct op.
  * @op: The operator.
  * @f: The associated function.
  */
-
 typedef struct op
 {
 	char *op;
