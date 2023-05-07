e2ca0e7480180667482d5c169778e277f2ff7569
KingDee715 <Davidaklamanu651@gmail.com>
2023-05-07 01:39:45 +0000

    0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,21 @@
  * Return: If b is NULL or contains chars not 0 or 1 - 0
  *         Otherwise - the converted number
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int fig = 0;
-	int i = 0;
+	int x = 0;
+	unsigned int val = 0;
 
-	if (b[i] == '\0')
+	if (!b)
 		return (0);
 
-	while ((b[i] == '0') || (b[i] == '1'))
+	for (; b[x]; x++)
 	{
-		fig <<= 1;
-		fig = fig + b[i] - '0';
-		i++;
+		if (b[x] < '0' || b[x] > '1')
+			return (0);
+		val = 2 * val + (b[x] - '0');
 	}
 
-	return (fig);
+	return (val);
 }
